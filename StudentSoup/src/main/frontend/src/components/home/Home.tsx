37d516b45252a3d7a09c8bfa84557c44502c967d
구승모 <seungmo@guseungmoui-noteubug.local>
2023-02-13 17:20:00 +0900

    delete : 테블릿 화면 삭제

--- StudentSoup/src/main/frontend/src/components/home/Home.tsx
+++ StudentSoup/src/main/frontend/src/components/home/Home.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Desktop, Mobile, Tablet } from '../../mediaQuery';
+import { Desktop, Mobile } from '../../mediaQuery';
 
 const Home = () => {
   return (
     <>
       <Desktop>
-        <p>데스크탑 화면</p>
+        <div>데스크탑 화면 입니다</div>
       </Desktop>
-      <Tablet>
-        <p>테블릿 화면</p>
-      </Tablet>
       <Mobile>
-        <p>모바일화면</p>
+        <div>모바일 화면 입니다</div>
       </Mobile>
     </>
   );
