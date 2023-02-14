4360ffe3ee04b2396ae83a0488a69d5c1e049a51
구승모 <seungmo@guseungmoui-noteubug.local>
2023-02-14 18:06:41 +0900

    FEAT : 배경화면 추가

--- StudentSoup/src/main/frontend/src/App.tsx
+++ StudentSoup/src/main/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './components/home/Home';
+import './App.css';
 import Router from './Router';
 
 const App = () => {
