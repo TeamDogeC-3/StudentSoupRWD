758937f4e01f19cc49f1791bf8b0c9074c8692b9
구승모 <seungmo@guseungmoui-noteubug.local>
2023-02-14 18:07:22 +0900

    FEAT: 메인화면 추가

--- StudentSoup/src/main/frontend/src/components/home/Home.tsx
+++ StudentSoup/src/main/frontend/src/components/home/Home.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import MainNavbar from '../common/mainNavbar';
 import { Desktop, Mobile } from '../../mediaQuery';
-
+import './home.css';
+import MainLogo_white from '../../img/mainLogo_white.svg';
+import Search_icon from '../../img/search_icon.svg';
 const Home = () => {
   return (
     <>
+      <MainNavbar />
       <Desktop>
-        <div>데스크탑 화면 입니다</div>
+        <div className="hero-text">
+          <img src={MainLogo_white} />
+          <p>대학생들을 위한</p>
+          <h2 className="link-texts">대학 주변 맛집 추천</h2>
+          <div className="school_search_bar">
+            <img src={Search_icon} />
+            <input placeholder="입력"></input>
+            <button>검색</button>
+          </div>
+        </div>
       </Desktop>
       <Mobile>
-        <div>모바일 화면 입니다</div>
+        <div>모바일</div>
       </Mobile>
     </>
   );
