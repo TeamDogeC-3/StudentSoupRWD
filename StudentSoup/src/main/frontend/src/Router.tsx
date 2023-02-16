27b1db54cd12db8399ec73f17f804f4d919ebe6b
KanghoYoo <kangsick98@gmail.com>
2023-02-16 17:50:24 +0900

    Add: 로그인 라우터 추가

--- StudentSoup/src/main/frontend/src/Router.tsx
+++ StudentSoup/src/main/frontend/src/Router.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/home/home';
+import Home from './components/home/Home';
+import Login from './components/login/Login';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
