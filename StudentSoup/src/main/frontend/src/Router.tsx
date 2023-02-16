29631fdd4b0fa82566746da7477944deba089c90
구승모 <seungmo@guseungmoui-noteubug.local>
2023-02-16 18:20:49 +0900

    style : 홈 이름 변경

--- StudentSoup/src/main/frontend/src/Router.tsx
+++ StudentSoup/src/main/frontend/src/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './components/home/home';
+import Home from './components/home/Home';
 
 const Router = () => {
   return (
